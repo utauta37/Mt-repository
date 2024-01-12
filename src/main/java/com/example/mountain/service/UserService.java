d38dc03d00659e822ca640180a5706cf542d67ff
mina <mina@ryo-S540>
2024-01-12 22:38:21 +0900

    ユーザ新規登録　パスワードのハッシュ化

--- src/main/java/com/example/mountain/service/UserService.java
+++ src/main/java/com/example/mountain/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.mountain.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.mountain.entity.MtUser;
@@ -20,7 +21,7 @@ public class UserService {
 	private final UserMapper mapper;
 	
 	/** PasswordEncoder */
-	//private final PasswordEncoder passwordEncoder;
+	private final PasswordEncoder passwordEncoder;
 	
 
 	/** ユーザー新規登録
@@ -28,6 +29,8 @@ public class UserService {
 	 * @param signupForm 入力情報
 	 */
 	public void saveUser(SignupForm signupForm) {
+		//パスワードをハッシュ化して、saveUser()に渡すオブジェクトにセット。
+		signupForm.setPassword(passwordEncoder.encode(signupForm.getPassword()));
 		mapper.saveUser(signupForm);
 		
 		//TODO パスワードをハッシュ化
